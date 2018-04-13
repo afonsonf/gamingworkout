f816e5b1a060e06809eb4e814e24480edf6a5f94
Afonso Fernandes <afonsofernandes@netcabo.pt>
2018-04-13 21:01:16 +0100

    mainwindow

--- gamingWorkout/mainwindow.cpp
+++ gamingWorkout/mainwindow.cpp
@@ -2,18 +2,35 @@
 #include "ui_mainwindow.h"
 
 #include <QPushButton>
+#include <QTextEdit>
+#include <QLabel>
+#include <QWidget>
 #include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    int j=0;
-    for(int i=0;i<500;i++){
-        QPushButton *p = new QPushButton;
-        qDebug() << j << " " << i%3;
-        if(i%3 == 0 && i>2) j++;
-        ui->gridLayout->addWidget(p,j,i%3);
+    for(int i=0;i<10;i++){
+        QPixmap pixmap(":/icon/semaforo/img/semaforo.png");
+        QLabel *img = new QLabel;
+        img->setPixmap(pixmap.scaled(200,200,Qt::IgnoreAspectRatio, Qt::FastTransformation));
+        img->setFixedHeight(200);
+        img->setFixedWidth(200);
+        QPushButton *b = new QPushButton("play");
+        QTextEdit *t = new QTextEdit("jogo");
+        QWidget *wv = new QWidget;
+        QWidget *wh = new QWidget;
+        QHBoxLayout *hl = new QHBoxLayout;
+        QVBoxLayout *vl = new QVBoxLayout;
+        vl->addWidget(t);
+        vl->addWidget(b);
+        wv->setLayout(vl);
+        hl->addWidget(img);
+        hl->addWidget(wv);
+        wh->setLayout(hl);
+        ui->gamesL->addWidget(wh);
     }
 
     //comentario afonso
