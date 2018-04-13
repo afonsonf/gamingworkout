46e758f5b2c5adba8b098503988187b2a2cd3257
Afonso Fernandes <afonsofernandes@netcabo.pt>
2018-04-13 19:31:30 +0100

    for group

--- gamingWorkout/mainwindow.cpp
+++ gamingWorkout/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    int j=0;
+    for(int i=0;i<10;i++){
+        QPushButton *p = new QPushButton;
+        ui->gridLayout->addWidget(p,j,i%3);
+        if(j%3 == 0 && !j) j++;
+    }
 }
 
 MainWindow::~MainWindow()
